6c196bfa3b8828c576c5ec6c0ecdd82e25c2281e
Yoona17 <96585672+Yoona17@users.noreply.github.com>
2022-12-10 06:08:45 +0530

    Add files via upload

---
+++ event-management-system-master/EventManagementSystems/locations2.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Bootstrap Web Design</title>
+        <?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+        <?php require 'utils/scripts.php'; ?><!--js links. file found in utils folder-->
+    </head>
+    <body>
+        <?php require 'utils/header.php'; ?><!--header content. file found in utils folder-->
+        <div class="content"><!--body content holder-->
+            <div class="container">
+                <div class="col-md-12"><!--body content title holder with 12 grid columns-->
+                    <h1>Our Venues</h1><!--body content title-->
+                </div>
+            </div>
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+			
+            <div class="row"><!--location content -->
+                <section>
+                    <div class="container">
+                        <div class="col-md-4"><!--image holder with 4 grid column-->
+                            <img src="images/urbanxchange.jpg" class="img-responsive">
+                        </div>
+                        <div class="modal fade" id="urbanxchange" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><!--location modal content-->
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header"><!--modal header-->
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><!--close button on top-->
+                                        <h4 class="modal-title" id="myModalLabel">Grand Dining Room</h4><!--modal title-->
+                                    </div>
+                                    <div class="row">
+                                        <div class="modal-body"><!--modal content-->
+                                            <div class="col-md-6"><!--image holder with 6 grid column-->
+                                                <img src="images/urbanxchange.jpg" class="img-responsive">
+                                            </div>
+                                            <div class="modaltext1 col-md-6">
+                                                <h5>THE ULTIMATE PRIVATE DINING EXPERIENCE</h5><!--content title-->
+                                                
+                                            </div> 
+                                            <div class="modaltext2 col-md-12">
+                                                
+                                            </div>
+                                        </div><!--modal body-->
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-default closebtn" data-dismiss="modal">Close</button><p><!--close button-->
+                                    </div><!--modal footer-->
+                                </div><!--modal content div-->
+                            </div><!--modal dialog div-->
+                        </div><!--modal div-->
+                        <div class="subcontent col-md-8"><!--location non modal content -->
+                            <h1 class="title">Grand Dining Room</h1><!--location title-->
+                            <p class="location"><!--location secondary content-->
+                            Leela Palace
+                            </p>
+                            <p class="definition"><!--content body-->
+                            Amazing Restaurant 
+                            </p>
+                            <hr class="customline3"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#urbanxchange"><!--button for modal trigger-->
+                            More Details  <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span><!--glyphicon--> 
+                            </button>
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+
+            <div class="row"><!--location content -->
+                <section>
+                    <div class="container">
+                        <div class="col-md-4"><!--image holder with 4 grid column-->
+                            <img src="images/ananasbar.jpg" class="img-responsive">
+                        </div>
+                        <div class="modal fade" id="ananasbar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><!--location modal content-->
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header"><!--modal header-->
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><!--close button on top-->
+                                        <h4 class="modal-title" id="myModalLabel">JW Marriot/h4><!--modal title-->
+                                    </div>
+                                    <div class="row">
+                                        <div class="modal-body"><!--modal content-->
+                                            <div class="col-md-6"><!--image holder with 6 grid column-->
+                                                <img src="images/ananasbar.jpg" class="img-responsive">
+                                            </div>
+                                            <div class="modaltext1 col-md-6">
+                                                <h5>Bra and Brasserie</h5><!--content title-->
+                                                <p>
+                                                Club, Bar & Brasserie
+                                                </p>
+                                            </div> 
+                                            <div class="modaltext2 col-md-12">
+                                                
+                                            </div>
+                                        </div><!--modal body-->
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-default closebtn" data-dismiss="modal">Close</button><p>
+                                    </div><!--modal footer-->
+                                </div><!--modal content div-->
+                            </div><!--modal dialog div-->
+                        </div><!--modal div-->
+                        <div class="subcontent col-md-8"><!--location non modal content -->
+                            <h1 class="title">Bar & Brasserie</h1><!--location title-->
+                            <p class="location">JW Marriot</p><!--location secondary content-->
+                            <p class="definition">
+                            Bar & Brasserie is the best hangout spot for friends and drinks
+                            </p>
+                            <hr class="customline3"><!--css modified horizontal line--> 
+                            <button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#ananasbar"><!--button for modal trigger-->
+                            More Details  <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span><!--glyphicon--> 
+                            </button>
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+
+            <div class="row"><!--location content -->
+                <section>
+                    <div class="container">
+                        <div class="col-md-4"><!--image holder with 4 grid column-->
+                            <img src="images/argyle.jpg" class="img-responsive">
+                        </div>    
+                        <div class="modal fade" id="argyle" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><!--location modal content-->
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header"><!--modal header-->
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><!--close button on top-->
+                                        <h4 class="modal-title" id="myModalLabel">The Casket</h4><!--modal title-->
+                                    </div>
+                                    <div class="row">
+                                        <div class="modal-body"><!--modal content-->
+                                            <div class="col-md-6"><!--image holder with 6 grid column-->
+                                                <img src="images/argyle.jpg" class = "img-responsive">
+                                            </div>
+                                            <div class="modaltext1 col-md-6">
+                                                <h5>caskets</h5><!--content title-->
+                                                
+                                            </div> 
+                                            <div class="modaltext2 col-md-12">
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-default closebtn" data-dismiss="modal">Close</button><p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="subcontent col-md-8">
+                            <h1 class="title">The Casket</h1>
+                            <p class="location">Leela Palace</p>
+                            <p class="definition">the best bar in town</p>
+                            <hr class="customline3">   
+                            <button type="button" class="btn btn-default btn-md" data-toggle = "modal" data-target = "#argyle">
+                            More Details  <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span> 
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+
+            <div class="row"><!--location content -->
+                <section>
+                    <div class="container">
+                        <div class="col-md-4"><!--image holder with 4 grid column-->
+                            <img src="images/munich.jpg" class = "img-responsive">
+                        </div>
+                        <div class="modal fade" id="munich" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><!--location modal content-->
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header"><!--modal header-->
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel">Cafe Noice</h4><!--modal title-->
+                                    </div>
+                                    <div class="row">
+                                        <div class="modal-body"><!--modal content-->
+                                            <div class="col-md-6"><!--image holder with 6 grid column-->
+                                                <img src="images/munich.jpg" class = "img-responsive">
+                                            </div>
+                                            <div class="modaltext1 col-md-6">
+                                                <h5>THE BEST</h5>
+                                                <p>
+                                                
+                                                </p>
+                                            </div> 
+                                            <div class="modaltext2 col-md-12">
+                                                
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-default closebtn" data-dismiss="modal">Close</button><p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="subcontent col-md-8">
+                            <h1 class="title">Noice Cafe</h1>
+                            <p class="location">Hotel Ashok</p>
+                            <p class="definition">This fantastic venue is our latest venture.</p>
+                            <hr class="customline3">   
+                            <button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#munich">
+                            More Details  <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span> 
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <?php require 'utils/footer.php'; ?>
+    </body>
+</html>
---
+++ event-management-system-master/EventManagementSystems/utils/footer.php
@@ -0,0 +1,38 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>S</strong><span class = "small footerSubtext">ujitha<BR></span>
+                        <strong>A</strong><span class = "small footerSubtext">shmita<BR></span>
+                        <strong>A</strong><span class = "small footerSubtext">rpitha<BR></span>
+                        <strong>D</strong><span class = "small footerSubtext">himanth<BR></span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Doddanekundi, Bengaluru
+                        &copy; SAAD Events, 2022.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    SAAD multimedia<br>
+                    SAAD Computing 2015-2016.<br>
+                    SAAD, 993822
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
